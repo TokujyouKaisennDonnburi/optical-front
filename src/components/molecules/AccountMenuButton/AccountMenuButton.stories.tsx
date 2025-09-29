e33bda8f15377b32bc04cd18e56d23464427e09c
N_A <kd1338248@st.kobedenshi.ac.jp>
2025-09-30 02:16:33 +0900

    feat/issue8: アカウントメニュー内にアイコン,ユーザー名,メアドを表示

--- src/components/molecules/AccountMenuButton/AccountMenuButton.stories.tsx
+++ src/components/molecules/AccountMenuButton/AccountMenuButton.stories.tsx
@@ -1,27 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { AccountMenuButton } from "./AccountMenuButton";
-import { useState } from "react";
 
 const meta: Meta<typeof AccountMenuButton> = {
   title: "Molecules/AccountMenuButton",
   component: AccountMenuButton,
-  tags: ["autodocs"],
   args: {
-    name: "user name",
-    avatarUrl: "https://i.pravatar.cc/150?img=3",
+    name: "John Doe",
+    avatarUrl: "https://i.pravatar.cc/100",
     avatarSizeClass: "h-12 w-12",
   },
 };
 export default meta;
-
 type Story = StoryObj<typeof AccountMenuButton>;
 
-export const Default: Story = {
-  render: (args) => {
-    return (
-      <div style={{ width: "100px" }}>
-        <AccountMenuButton {...args} onClick={() => alert("クリックされました")} />
-      </div>
-    );
+export const Default: Story = {};
+
+export const WithoutAvatar: Story = {
+  args: {
+    avatarUrl: undefined,
+    name: "Jane Doe",
   },
 };
