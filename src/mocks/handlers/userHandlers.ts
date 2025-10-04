776965684612ea1917fb30d2bd517ca440189fbc
N_A <kd1338248@st.kobedenshi.ac.jp>
2025-10-05 00:06:20 +0900

    feat/issue8: mocksとhooksのユーザー情報関連ファイルにコメントを追加

--- src/mocks/handlers/userHandlers.ts
+++ src/mocks/handlers/userHandlers.ts
@@ -2,8 +2,11 @@ import { http, HttpResponse } from "msw";
 
 import { userMock } from "@/mocks/data/user";
 
+// ユーザー情報取得エンドポイントのモックハンドラ
 export const userHandlers = [
   http.get("/api/user", () => {
+    
+    // モックデータを返す
     return HttpResponse.json(userMock);
   }),
 ];
