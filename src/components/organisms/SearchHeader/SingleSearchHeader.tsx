17ffde5a43a7bc5e54ac4d8cef3adda07d6c4616
y_k <ka.20.zu.04@gmail.com>
2025-12-05 13:11:42 +0900

    feat/issue26: 検索バーに通知バッチを追加

--- src/components/organisms/SearchHeader/SingleSearchHeader.tsx
+++ src/components/organisms/SearchHeader/SingleSearchHeader.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Button } from "@/components/atoms/Button";
 import { BadgeButton } from "@/components/molecules/BadgeButton";
 import { SearchInput } from "@/components/molecules/SearchInput/SearchInput";
+import { Badge } from "lucide-react";
+import { BadgeButton } from "@/components/molecules/BadgeButton";
 
 type SingleSearchHeaderProps = {
   searchValue?: string;
