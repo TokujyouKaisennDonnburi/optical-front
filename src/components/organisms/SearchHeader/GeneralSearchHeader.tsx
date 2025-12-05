db60ba4e32d7b32d07694731359d4bca431ac754
y_k <ka.20.zu.04@gmail.com>
2025-12-05 13:11:42 +0900

    feat/issue26: 検索バーに通知バッチを追加

--- src/components/organisms/SearchHeader/GeneralSearchHeader.tsx
+++ src/components/organisms/SearchHeader/GeneralSearchHeader.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/atoms/Button";
 import { BadgeButton } from "@/components/molecules/BadgeButton";
 import { MultiSelectDropdown } from "@/components/molecules/MultiSelectDropdown/MultiSelectDropdown";
 import { SearchInput } from "@/components/molecules/SearchInput/SearchInput";
+import { BadgeButton } from "@/components/molecules/BadgeButton";
 
 type LabeledOption = { label: string; value: string };
 
